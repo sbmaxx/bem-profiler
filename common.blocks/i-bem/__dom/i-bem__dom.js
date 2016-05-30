7eff4a3c690449f9e12627d2d91e5dbc1d5e1e60
sbmaxx <sbmaxx@yandex-team.ru>
2016-05-30 12:25:38 +0300

    Добавил window.performance.now

--- common.blocks/i-bem/__dom/i-bem__dom.js
+++ common.blocks/i-bem/__dom/i-bem__dom.js
@@ -6,8 +6,7 @@
     }
 
     var data,
-        timeout,
-        getDelta;
+        timeout;
 
     // контейнер для отладочной информации
     data = {
@@ -22,17 +21,35 @@
     timeout = 3000;
 
     // помощник для подсчёта времени выполнения метода
-    getDelta = function(fn, ctx, args) {
-        var start = (new Date()).getTime();
+    function getDelta(fn, ctx, args) {
+        var start = window.performance.now();
         fn.apply(ctx, args);
-        return (new Date()).getTime() - start;
-    };
+        return window.performance.now() - start;
+    }
+
+    window.performance = (window.performance || {
+        offset: Date.now(),
+        now: function now(){
+            return Date.now() - this.offset;
+        }
+    });
 
     BEM.decl('i-bem', {}, {
 
         // публичный помощник для получения свойства из замыкания
         profiler: function(block) {
-            return (block && data[block]) ? data[block] : data;
+            var output = (block && data[block]) ? data[block] : data;
+
+            // после window.perfomance у нас получается много знаков после запятой
+            // оставляем два — получается строка, которую потом приводим снова к числу,
+            // чтобы табличка была чище (без кавычек)
+            Object.keys(output).forEach(function(block) {
+                Object.keys(output[block]).forEach(function(field) {
+                    output[block][field] = parseFloat(output[block][field].toFixed(2));
+                });
+            });
+
+            return output;
         }
 
     });
@@ -44,21 +61,16 @@
         // внутренние методы инициализации (парсинг параметров и прочее) опускаем
         __constructor: function() {
 
-            var delta,
-                sync,
-                async,
-                block;
-
-            sync = getDelta(this.__base, this, arguments);
+            var syncTime = getDelta(this.__base, this, arguments);
 
             // синхронно вызываем добавленные блоком методы через afterCurrentEvent,
             // если этого не делать, то время выполнения блока будет нечестным
-            async = getDelta(this.__self._runAfterCurrentEventFns, this.__self, arguments);
+            var asyncTime = getDelta(this.__self._runAfterCurrentEventFns, this.__self, arguments);
 
-            delta = sync + async;
+            var delta = syncTime + asyncTime;
 
             // имя блока доступно только после вызова конструктора по умолчанию
-            block = this.__self.getName();
+            var block = this.__self.getName();
 
             if (typeof data[block] === 'undefined') {
                 data[block] = {
@@ -70,8 +82,8 @@
 
             // по факту это время инициализации блока и его внутренних методов
             data[block].time += delta;
-            data[block].sync += sync;
-            data[block].async += async;
+            data[block].sync += syncTime;
+            data[block].async += asyncTime;
 
             return this;
 
@@ -80,14 +92,6 @@
     }, {
 
         init: function() {
-            data.__total.time += getDelta(this.__base, this, arguments);
-            data.__total.sync = 0;
-            // по идее, здесь уже не должно быть ничего асинхронного,
-            // т.к. мы форсируем вызов afterCurrentEvent во время инициализации каждого блока
-            data.__total.async += getDelta(this._runAfterCurrentEventFns, this, arguments);
-        },
-
-        asyncInit: function() {
             data.__total.time += getDelta(this.__base, this, arguments);
             data.__total.sync = 0;
             // по идее, здесь уже не должно быть ничего асинхронного,
